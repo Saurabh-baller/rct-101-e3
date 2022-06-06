5a0f9862baae9387969a5d85b8a4588432932b7c
Saurabh-baller <102128315+Saurabh-baller@users.noreply.github.com>
2022-06-06 17:07:24 +0530

    first commit

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import { useContext } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { AuthContext } from "../../context/AuthContext";
+
+
+
+const Navbar = () => {
+  const { isAuth, logout } = useContext(AuthContext);
+  const navigate = useNavigate();
+  const handleChange = () => {
+  
+    if (isAuth) {
+      logout();
+     
+    } else {
+    
+      navigate("/login");
+    }
+  };
+  return (
+    <div data-cy="navbar">
+     
+      <a data-cy="navbar-home-link" path="">Logo</a>
+      <span data-cy="navbar-cart-items-count">{/* count here */} 0</span>
+      <button data-cy="navbar-login-logout-button" onClick={handleChange}>
+    {isAuth ? "Logout" : "Login"}
+      </button>
+    </div>
+  );
+};
+
+export default Navbar;
